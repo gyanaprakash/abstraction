2d381bbac92c12c495e30d2c20127c45e2033308
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-11-13 10:24:07 +0530

    Initial Commit

---
+++ abstraction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  abstraction
+//
+//  Created by Bsetecip10 on 13/11/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
